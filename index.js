bdfce9ff261ec51eda813b3f30401554c8182023
dev-rahim <rahimuddin5915@gmail.com>
2023-10-12 22:06:14 +0600

    created an api for bloge  post with jwt varification

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-
+const jwt = require("jsonwebtoken");
 app.use(express.json());
 app.use(cors());
 
@@ -20,6 +20,35 @@ const client = new MongoClient(uri, {
 });
 
 const userCollection = client.db("SocialMedia").collection("userCollection");
+const postCollection = client.db("SocialMedia").collection("userCollection");
+
+const verifyJwt = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  console.log(
+    "🚀 ~ file: index.js:27 ~ verifyJwt ~ authorization:",
+    authorization
+  );
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  const token = authorization.split(",")[1];
+  if (!token) {
+    console.log("token not found");
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      console.log(err);
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    console.log({ decoded });
+    req.decoded = decoded;
+    next();
+  });
+};
 
 async function run() {
   try {
@@ -49,6 +78,21 @@ async function run() {
       user.time = Date.now();
       const result = await userCollection.insertOne(user);
       console.log(result);
+      res.send(result);
+    });
+
+    app.post("/post", verifyJwt, async (req, res) => {
+      const post = req.body;
+
+      const userId = req.query.uid;
+      const decodedUid = req.decoded.uid;
+      console.log({ decodedEmail: decodedUid });
+      if (userId !== decodedUid) {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden access" });
+      }
+      const result = await postCollection.insertOne(post);
       res.send(result);
     });
   } finally {
