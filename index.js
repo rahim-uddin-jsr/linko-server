4e0666dabfc004a70b5eb3d63717acffc1e28bfa
dev-rahim <rahimuddin5915@gmail.com>
2023-10-12 20:24:04 +0600

    jwt api route created done

--- index.js
+++ index.js
@@ -28,6 +28,15 @@ async function run() {
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
+
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "2 days",
+      });
+      res.send({ token });
+    });
+
     app.get("/", async (req, res) => {
       res.send("ok");
     });
